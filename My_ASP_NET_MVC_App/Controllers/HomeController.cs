833a3359583aff7dc93aaf1c20becc7aa92cf0fd
Zaripov Shamil <isanbet@mail.ru>
2018-10-18 15:05:12 +0500

    MyFirstCommit
    ыы

--- My_ASP_NET_MVC_App/Controllers/HomeController.cs
+++ My_ASP_NET_MVC_App/Controllers/HomeController.cs
@@ -3,11 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using My_ASP_NET_MVC_App.Models;
 
 namespace My_ASP_NET_MVC_App.Controllers
 {
     public class HomeController : Controller
     {
+        // создаем контекст данных
+        BookContext db = new BookContext();
+
+        public ActionResult Index()
+        {
+            // получаем из бд все объекты Book
+            IEnumerable<Book> books = db.Books;
+            // передаем все объекты в динамическое свойство Books в ViewBag
+            ViewBag.Books = books;
+            // возвращаем представление
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Buy(int id)
+        {
+            ViewBag.BookId = id;
+            return View();
+        }
+        [HttpPost]
+        public string Buy(Purchase purchase)
+        {
+            purchase.Date = DateTime.Now;
+            // добавляем информацию о покупке в базу данных
+            db.Purchases.Add(purchase);
+            // сохраняем в бд все изменения
+            db.SaveChanges();
+            return "Спасибо," + purchase.Person + ", за покупку!";
+        }
+
+        /*
         public ActionResult Index()
         {
             return View();
@@ -26,5 +58,6 @@ namespace My_ASP_NET_MVC_App.Controllers
 
             return View();
         }
+        */
     }
 }
--- My_ASP_NET_MVC_App/Global.asax.cs
+++ My_ASP_NET_MVC_App/Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using My_ASP_NET_MVC_App.Models;
+using System.Data.Entity;
 
 namespace My_ASP_NET_MVC_App
 {
@@ -12,6 +14,8 @@ namespace My_ASP_NET_MVC_App
     {
         protected void Application_Start()
         {
+            Database.SetInitializer(new BookDbInitializer());
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
